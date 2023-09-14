e081b8abd3572a08bf9b21b22cb62f99e3a8cda1
AntiRain <dailybreak3@gmail.com>
2023-09-14 13:48:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class player:
     hand_token = {'white': 0, 'blue':0, 'green':0, 'red':0, 'black':0, 'yellow':0}
     discount = {'white': 0, 'blue':0, 'green':0, 'red':0, 'black':0, 'yellow':0}
     card = []
+    reservation = []
 
 class develope_card:
     def __init__(self, white, blue, green, red, black, score, discount):
@@ -13,8 +14,3 @@ class develope_card:
         self.discount = discount
 
 player1 = player
-player1.hand_token['red'] += 2
-print(player1.hand_token)
-
-card1 = develope_card(0,3,3,5,3,3,'blue')
-print(card1.price, card1.score, card1.discount)
